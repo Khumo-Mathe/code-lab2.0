007c6c30f9833c1a84e65d351d4cd1ff0947e533
Khumo-Mathe <kmathe941@gmail.com>
2025-07-15 11:09:15 +0200

    pycommit22

---
+++ Python/hello.py/controlflowbasic.py
@@ -0,0 +1,10 @@
+#a code that checks if a user is valid for a drivers licenece
+
+name = input("what is your name? ")
+age = int(input(" hi {0} how old are you? " .format(name)))
+print("you are {} years old"  .format(age))
+
+if age >= 18:
+    print("you are legible to drive {0} ".format(name))
+else:
+    print("you are under age {1} ,please come back in {0} years ".format(18-age , name))
