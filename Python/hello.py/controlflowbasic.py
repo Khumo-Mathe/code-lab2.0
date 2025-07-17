cbaf4208043005849412fc1e2bb85324f9687a44
Khumo-Mathe <kmathe941@gmail.com>
2025-07-17 10:48:09 +0200

    pycommit33

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,11 +1,9 @@
 #program that checks idf a given number is "special",a number is special its divible by 5 or 3 
 #not both at the same time
-print("insert number")
-number = int(input())
 
-if (number % 3 == 0 and number % 5==0):
-     print("not special")
-elif (number % 3 == 0 or number % 5==0):
+number = int(input("insert number: "))
+
+if (number % 3 == 0 or number % 5==0) and not (number % 3 == 0 and number % 5==0):
      print("special")
 else:
      print("not special")
