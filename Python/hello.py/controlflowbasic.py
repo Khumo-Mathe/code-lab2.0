d0f80c8671e6da35c2c118584ae4382f466f13b4
Khumo-Mathe <kmathe941@gmail.com>
2025-07-21 15:18:34 +0200

    pycommit46

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -4,13 +4,14 @@ grade = int(input("Enter your grade: "))
 
 def categorize_grade(grade):
     if grade >= 75:
-        return "A"
-    elif grade >= 80:
-        return "B"
-    elif grade >= 70:
-        return "C"
-    elif grade >= 60:
-        return "D"
+        return "distinction"
+    elif grade >= 60 and grade < 75:
+        return "merit"
+    elif grade >= 50 and grade < 60:
+        return "pass"
+    elif  grade < 50:
+        return "fail"
     else:
         return "F"
+print("Your grade is:", categorize_grade(grade))
   
