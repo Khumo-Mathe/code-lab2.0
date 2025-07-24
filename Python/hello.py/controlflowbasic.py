895cd69da6430861ac95e22568b275c71cafdf9d
Khumo-Mathe <kmathe941@gmail.com>
2025-07-24 12:12:19 +0200

    pycommit47

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,17 +1,6 @@
-#grade categorizer
+#BASIC LOOP
 
-grade = int(input("Enter your grade: "))
+name = "khumo"
 
-def categorize_grade(grade):
-    if grade >= 75:
-        return "distinction"
-    elif grade >= 60 and grade < 75:
-        return "merit"
-    elif grade >= 50 and grade < 60:
-        return "pass"
-    elif  grade < 50:
-        return "fail"
-    else:
-        return "F"
-print("Your grade is:", categorize_grade(grade))
-  
+for character in name:
+    print(character)
