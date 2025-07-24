5f543ade54cd0dd476a3b3237df397278dc2288f
Khumo-Mathe <kmathe941@gmail.com>
2025-07-24 12:37:24 +0200

    pycommit48

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,6 +1,8 @@
-#BASIC LOOP
 
-name = "khumo"
 
-for character in name:
-    print(character)
+quote = "Hello, World! This is a test string with some capitals."
+
+#use for loop and if statemet to print capitals in the above qoute
+for char in quote:
+    if char.isupper():
+        print(char)
