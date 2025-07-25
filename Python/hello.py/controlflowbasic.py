fe6933dc454cf66102a4d40f799fbea4b01a9d16
Khumo-Mathe <kmathe941@gmail.com>
2025-07-25 12:05:49 +0200

    pycommit50

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,4 +1,7 @@
 #roman to integer conversion
+import re
+
+
 class Solution:   
     def roman_to_int(self, s: str ) ->  int:
 
@@ -12,12 +15,14 @@ class Solution:
           'M': 1000
       }
       res = 0
-      prev_value = 0
-      for char in reversed(s):
-          curr_value = roman[char]
-          if curr_value < prev_value:
-              res -= curr_value
-          else:
-              res += curr_value
-          prev_value = curr_value
+      
+      for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
       return res
+
+s = input("Enter a Roman numeral: ")
+solution = Solution()
+print(solution.roman_to_int(s))
