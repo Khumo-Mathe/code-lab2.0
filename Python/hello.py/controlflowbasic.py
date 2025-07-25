4c619e3b903eba6c1d67ca47605e0293d63aa808
Khumo-Mathe <kmathe941@gmail.com>
2025-07-25 11:54:48 +0200

    pycommit49

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,8 +1,23 @@
+#roman to integer conversion
+class Solution:   
+    def roman_to_int(self, s: str ) ->  int:
 
-
-quote = "Hello, World! This is a test string with some capitals."
-
-#use for loop and if statemet to print capitals in the above qoute
-for char in quote:
-    if char.isupper():
-        print(char)
+      roman = {
+          'I': 1,
+          'V': 5,
+          'X': 10,
+          'L': 50,
+          'C': 100,
+          'D': 500,
+          'M': 1000
+      }
+      res = 0
+      prev_value = 0
+      for char in reversed(s):
+          curr_value = roman[char]
+          if curr_value < prev_value:
+              res -= curr_value
+          else:
+              res += curr_value
+          prev_value = curr_value
+      return res
