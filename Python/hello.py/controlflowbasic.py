276ea175f8681223a37ef0f7fb52888c5eee3723
Khumo-Mathe <kmathe941@gmail.com>
2025-07-28 11:50:20 +0200

    pycommit52

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,9 +1,3 @@
-fruits = ["apple", "banana", "cherry", "date", "elderberry"]
-
-fruit_to_find = "cherry"
-index = None
-
-for index in range(len(fruits)):
-    if fruits[index] == fruit_to_find:
-        print(f"Found {fruit_to_find} at index {index}")
-        break # If found, exit the loop                  
+while i <10:
+    print(i)
+    i += 1
