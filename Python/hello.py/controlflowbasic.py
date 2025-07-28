b64f6d0b52c7863e9c7cea4cb657cf0db3e94182
Khumo-Mathe <kmathe941@gmail.com>
2025-07-28 10:50:16 +0200

    pycommit52

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,29 +1,7 @@
-#roman to integer conversion
-import re
+for i in range(11):
+    print("i is now {}".format(i))
+    if i == 5:
+        print("i is equal to 5, breaking the loop")
+        break   
 
-
-class Solution:
-
-    def roman_to_int(self, s: str) -> int:
-
-      roman = {
-          'I': 1,
-          'V': 5,
-          'X': 10,
-          'L': 50,
-          'C': 100,
-          'D': 500,
-          'M': 1000
-      }
-      res = 0
-      
-      for i in range(len(s)):
-            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
-                res -= roman[s[i]]
-            else:
-                res += roman[s[i]]
-      return res
-
-s = input("Enter a Roman numeral: ")
-solution = Solution()
-print(solution.roman_to_int(s))
+  
