ac3122b75106ae3bfb65574be74d0893535b581d
Khumo-Mathe <kmathe941@gmail.com>
2025-07-29 14:07:09 +0200

    pycommit61

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,22 +1,8 @@
-low = 1
-high = 1000
+number=5
+answer = 0
 
-print(f"please think of a number between {low} and {high}")
-input("please press enter to start")
+for i in range(8):
+    answer+= number
 
-guess =1 
-
-while True:
-    guess = low + (high - low) // 2
-    high_low = input(f"Is {guess} too high (H), too low (L), or correct (C)? ").strip().upper()
-    if high_low == "H":
-        print(f"Your number is between {low} and {guess - 1}")
-    
-    elif high_low == "L":
-        print(f"Your number is between {guess + 1} and {high}")
-        low = guess + 1
-    elif high_low == "C":
-        print(f"Your number is {guess}!")
-        break
-    else:
-        print("Please enter H, L, or C")
+print("The answer is:", answer)
+# This code multiplies the number by 8 using a loop and prints the result.  
