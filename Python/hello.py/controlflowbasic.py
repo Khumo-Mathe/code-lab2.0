bb01f6b1b41cd3a0b356f8649b52a236fb32bef6
Khumo-Mathe <kmathe941@gmail.com>
2025-07-29 12:33:44 +0200

    pycommit60

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,4 +1,22 @@
-i = 0
-while i < 10:
-    print(i)
-    i += 1
+low = 1
+high = 1000
+
+print(f"please think of a number between {low} and {high}")
+input("please press enter to start")
+
+guess =1 
+
+while True:
+    guess = low + (high - low) // 2
+    high_low = input(f"Is {guess} too high (H), too low (L), or correct (C)? ").strip().upper()
+    if high_low == "H":
+        print(f"Your number is between {low} and {guess - 1}")
+    
+    elif high_low == "L":
+        print(f"Your number is between {guess + 1} and {high}")
+        low = guess + 1
+    elif high_low == "C":
+        print(f"Your number is {guess}!")
+        break
+    else:
+        print("Please enter H, L, or C")
