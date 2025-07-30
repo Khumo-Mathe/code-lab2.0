b9efec7fd2f0c343a33787a856d72a472946143c
Khumo-Mathe <kmathe941@gmail.com>
2025-07-30 12:12:16 +0200

    pycommit65

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,20 +1,15 @@
-#basic inheritence 
+#basic polymorphism
 
-class Animal:
-   def speak(self):
-      return "some sound"
+class Duck:
+    def quack(self):
+        print("Quack!")
 
+class Human:
+    def quack(self):
+        print("Quack! Quack!, like a duck")
 
-class Dog(Animal):
-   def speak(self):
-     return "woof"
+def make_it_quack(thing):
+    thing.quack()
 
-class Cat(Animal):
-   def speak(self):
-     return "meow"
-
-def animal_sound(animal: Animal):  # polymorphic function
-   print(animal.speak()) # this will call the speak method of the specific animal type
-
-animal_sound(Dog())
-animal_sound(Cat())
+make_it_quack(Duck())  # Prints "Quack!"
+make_it_quack(Human())  # Prints "Quack! Quack!, like a duck"
