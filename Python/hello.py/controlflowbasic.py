58683bff815b3bd51739ae33692bf1431df3dfea
Khumo-Mathe <kmathe941@gmail.com>
2025-08-05 12:11:18 +0200

    list aliasing

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,9 +1,14 @@
-#basic list operations
+#list aliasing
 
-computer_parts = ['keyboard', 'mouse', 'monitor', 'CPU']
+sneaker_list = ['nike', 'adidas', 'puma', 'reebok', 'new_balance']
 
-#accessing elements
-for part in computer_parts:
-    print(part)
+another_list = sneaker_list
+print(sneaker_list)
 
-print(computer_parts[3])
+print(id(another_list))
+print(id(sneaker_list))
+
+sneaker_list += ['converse', 'vans']
+print(sneaker_list)
+print(another_list)  # This will also show the updated list since both variables point to the same object
+print(id(another_list))  # id remains the same, showing that both variables point to the same list object
