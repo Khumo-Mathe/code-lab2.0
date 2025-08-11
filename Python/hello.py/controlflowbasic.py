dbc60463c014dc0e0bcb653a98b4844a1ff5431d
Khumo-Mathe <kmathe941@gmail.com>
2025-08-11 15:45:58 +0200

    refactor: replace list aliasing example with FizzBuzz implementation

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,15 +1,14 @@
-#list aliasing
 
-sneaker_list = ['nike', 'adidas', 'puma', 'reebok', 'new_balance']
-
-another_list = sneaker_list
-print(sneaker_list)
-
-print(id(another_list))
-print(id(sneaker_list))
-
-sneaker_list += ['converse', 'vans']
-print(sneaker_list)
-print(another_list)  # This will also show the updated list since both variables point to the same object
-print(id(another_list))  # id remains the same, showing that both variables point to the same list object
-print(id(sneaker_list))  # id remains the same, showing that both variables point to the same list object
+Number = int(input("Enter a number: "))
+if Number > 100:
+    print("number too high, enter number lower than 100")
+else:
+    for num in range(1, Number + 1):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 5 == 0:
+            print("Buzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        else:
+            print(num)
