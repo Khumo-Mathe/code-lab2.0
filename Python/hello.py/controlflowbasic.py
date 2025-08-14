a43b6e88e471096466ef2c2f19dbf061c8704844
Khumo-Mathe <kmathe941@gmail.com>
2025-08-14 10:51:19 +0200

    simple dictionary function

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,27 +1,24 @@
-#count and say function
-def count_and_say(n: int)-> str:
-    if n == 1:
-        return "1"
-    current_term ="1"
-    
-    for _ in range(2 ,n + 1 ):
-        next_term = ""
-        count = 1
+def SouthAfrica():
+    capital_city={
+        "Gauteng" :"johannesburg",
+        "NorthWest" : "mahikeng",
+        "NorthenCape" : "kimberly",
+        "WesternCape" : "capetown",
+        "EasternCape" : "eastlondon",
+        "KwaZuluNatal" : "durban",
+        "Mpumalanga" : "nelspruit",
+        "FreeState" : "bloemfontein",
         
-        for i in range(1 , len(current_term)):
-            if current_term [i] == current_term[i - 1]:
-                count += 1
-            else:
-                next_term += str(count) + current_term[i - 1]
-                count = 1
-                
-        next_term += str(count) + current_term[-1]
-        current_term = next_term
-    
-    
-    return current_term
-n = int(input("enter a number  :"))
-result = count_and_say(n)
-print(result)
         
+    }
+    
+    province = input("enter province to get the capital city: ")
+    
+    capital = capital_city.get(province,"capital not found")
+    print(f"the capital of {province} is :  {capital}")
+SouthAfrica()  
+    
+    
+
+
         
