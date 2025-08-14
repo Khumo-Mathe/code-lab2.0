badae5179e89a0f1fbc7b75979ff48240b9784f8
Khumo-Mathe <kmathe941@gmail.com>
2025-08-14 09:43:00 +0200

    refactor: replace fizzbuzz function with count_and_say implementation

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,22 +1,27 @@
-# FizzBuzz function that returns a list of FizzBuzz results
-from typing import List
-
-
-def fizzbuzz( number : int) -> List[str]:
-    result = []
-
+#count and say function
+def count_and_say(n: int)-> str:
+    if n == 1:
+        return "1"
+    current_term ="1"
     
-
-    for num in range(1, number + 1):
-        if num % 5 == 0 and num % 3 == 0:
-            result.append("fizzbuzz")  
-        elif num % 3 == 0:
-            result.append("fizz")                            
-        elif num % 5 == 0:
-            result.append("buzz")
-        else:
-            result.append(str(num))
-    return result
-number = int(input("enter number: "))
-
-print(fizzbuzz(number))  # Print the result of the fizzbuzz function
+    for _ in range(2 ,n + 1 ):
+        next_term = ""
+        count = 1
+        
+        for i in range(1 , len(current_term)):
+            if current_term [i] == current_term[i - 1]:
+                count += 1
+            else:
+                next_term += str(count) + current_term[i - 1]
+                count = 1
+                
+        next_term += str(count) + current_term[-1]
+        current_term = next_term
+    
+    
+    return current_term
+n = int(input("enter a number  :"))
+result = count_and_say(n)
+print(result)
+        
+        
