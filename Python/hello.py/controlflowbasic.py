b50e6cbe8bf8146c07c7a3038fc4cb3f57e367d8
Khumo-Mathe <kmathe941@gmail.com>
2025-08-15 11:04:38 +0200

    two sum

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,13 +1,14 @@
-class  solution :
-    def largestgoodinterger(num:str):
-        max_good = ""
+class Solution:
+    def twoSum(self, nums: int, target: int) -> int:
+        seen = {}
         
-        for i in range(0,len(num)-3):
-            sub = num [i : i + 3]
-            if sub[0] == sub[1] == sub [2]:
-                if sub > max_good:
-                    max_good = sub
-        return max_good            
+        for i, num in enumerate(nums):
+            difference = target - num
+            if difference in seen:
+                return [seen[difference],i]
+            seen[num] = i
+        return[]         
+        
                 
         
         
