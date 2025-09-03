442f3f188d3af8112581ef34133dddb10400ca23
Khumo-Mathe <kmathe941@gmail.com>
2025-09-03 11:53:42 +0200

    split method

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,7 +1,5 @@
-class Solution:
-    def shuffle(self, nums, n):
-        result = []
-        for i in range(n):
-            result.append(nums[i])       # take from first half
-            result.append(nums[i + n])   # take from second half
-        return result
+#split method to split a string
+sentence = "THIS IS A TEST"
+
+SPLIT = sentence.split()    
+print(SPLIT)
