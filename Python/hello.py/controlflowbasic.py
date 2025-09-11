ff59cb9b07a4f086fd73f061c02c52e032ac7938
Khumo-Mathe <kmathe941@gmail.com>
2025-09-11 15:12:26 +0200

    binary search

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,9 +1,24 @@
-user_input = input ("please enter 3 numberss separated by commas: ")
+def binary_search(arr, target):
+    """Perform binary search on a sorted array.
 
-numbers = user_input.split(",")
-a = int(numbers[0])
-b = int(numbers[1]) 
-c = int(numbers[2])
+    Args:
+        arr (list): A sorted list of elements.
+        target: The element to search for.
 
-result = a + b - c
-print ("The result is: " + str(result))
+    Returns:
+        int: The index of the target element if found, otherwise -1.
+    """
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        mid_value = arr[mid]
+
+        if mid_value == target:
+            return mid
+        elif mid_value < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
