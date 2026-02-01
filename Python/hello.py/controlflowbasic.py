52e21901abe7d76182d5bcaf12cd86e6eec3d1c6
Khumo-Mathe <kmathe941@gmail.com>
2026-02-01 17:13:15 +0200

    leetcode: fizzbuzz

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,37 +1,17 @@
-def check_password_strength(password: str) -> dict:
-    result = {
-        "length": False,
-        "uppercase": False,
-        "lowercase": False,
-        "digit": False,
-        "special": False,
-        "is_strong": False
-    }
+def fizzbuzz(n: int) -> list:
+    result = []
 
-    # Rule 1: length check
-    if len(password) >= 8:
-        result["length"] = True
-
-    # Rule 2–5: character checks
-    for char in password:
-        if char.isupper():
-            result["uppercase"] = True
-        elif char.islower():
-            result["lowercase"] = True
-        elif char.isdigit():
-            result["digit"] = True
-        elif not char.isalnum():
-            result["special"] = True
-
-    # Final decision
-    result["is_strong"] = all([
-        result["length"],
-        result["uppercase"],
-        result["lowercase"],
-        result["digit"],
-        result["special"]
-    ])
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(i)
 
     return result
+
 
 
