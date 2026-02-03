77dece121eb377304b6818d491220e8eca188639
Khumo-Mathe <kmathe941@gmail.com>
2026-02-03 23:42:59 +0200

    Priority Task Scheduler (Heap-based)

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,26 +1,3 @@
-class LRUCache:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.cache = {}
-        self.order = []
 
-    def get(self, key):
-        if key not in self.cache:
-            return None
-
-        self.order.remove(key)
-        self.order.append(key)
-        return self.cache[key]
-
-    def put(self, key, value):
-        if key in self.cache:
-            self.order.remove(key)
-
-        elif len(self.cache) >= self.capacity:
-            lru_key = self.order.pop(0)
-            del self.cache[lru_key]
-
-        self.cache[key] = value
-        self.order.append(key)
 
 
