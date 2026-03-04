8a3b2d3d6f6c62c3e0af2685c9aa23bb54edf607
Khumo-Mathe <kmathe941@gmail.com>
2026-03-05 01:01:31 +0200

    Refactor consistent hashing implementation into ConsistentHashRing class

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,25 +1,45 @@
 import hashlib
 import bisect
 
-def hash_value(key: str) -> int:
-    return int(hashlib.md5(key.encode()).hexdigest(), 16)
 
-def assign_server(servers: list[str], request_id: str) -> str:
-    """
-    Assigns a request to a server using Consistent Hashing
-    """
-    # Hash servers
-    server_hashes = sorted((hash_value(server), server) for server in servers)
+class ConsistentHashRing:
+    def __init__(self, replicas=3):
+        self.replicas = replicas
+        self.ring = {}
+        self.sorted_keys = []
 
-    # Hash request
-    request_hash = hash_value(request_id)
+    def _hash(self, key):
+        return int(hashlib.md5(key.encode()).hexdigest(), 16)
 
-    # Find first server clockwise
-    positions = [h[0] for h in server_hashes]
-    index = bisect.bisect(positions, request_hash)
+    def add_node(self, node):
+        for i in range(self.replicas):
+            virtual_node = f"{node}:{i}"
+            hashed_key = self._hash(virtual_node)
 
-    # Wrap around if needed
-    return server_hashes[index % len(servers)][1]
+            self.ring[hashed_key] = node
+            self.sorted_keys.append(hashed_key)
 
+        self.sorted_keys.sort()
+
+    def remove_node(self, node):
+        for i in range(self.replicas):
+            virtual_node = f"{node}:{i}"
+            hashed_key = self._hash(virtual_node)
+
+            del self.ring[hashed_key]
+            self.sorted_keys.remove(hashed_key)
+
+    def get_node(self, key):
+        if not self.ring:
+            return None
+
+        hashed_key = self._hash(key)
+
+        index = bisect.bisect(self.sorted_keys, hashed_key)
+
+        if index == len(self.sorted_keys):
+            index = 0
+
+        return self.ring[self.sorted_keys[index]]
 
 
