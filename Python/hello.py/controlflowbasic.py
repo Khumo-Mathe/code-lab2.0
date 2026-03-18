d0a6096921ad872683e250a361eb717b27c29412
Khumo-Mathe <kmathe941@gmail.com>
2026-03-18 17:51:14 +0200

    Implement BloomFilter class with add and might_contain methods

--- Python/hello.py/controlflowbasic.py
+++ Python/hello.py/controlflowbasic.py
@@ -1,45 +1,27 @@
 import hashlib
-import bisect
 
 
-class ConsistentHashRing:
-    def __init__(self, replicas=3):
-        self.replicas = replicas
-        self.ring = {}
-        self.sorted_keys = []
+class BloomFilter:
+    def __init__(self, size=1000, hash_count=3):
+        self.size = size
+        self.hash_count = hash_count
+        self.bit_array = [0] * size
 
-    def _hash(self, key):
-        return int(hashlib.md5(key.encode()).hexdigest(), 16)
+    def _hashes(self, item):
+        hashes = []
+        for i in range(self.hash_count):
+            hash_input = f"{item}-{i}"
+            hash_val = int(hashlib.md5(hash_input.encode()).hexdigest(), 16)
+            hashes.append(hash_val % self.size)
+        return hashes
 
-    def add_node(self, node):
-        for i in range(self.replicas):
-            virtual_node = f"{node}:{i}"
-            hashed_key = self._hash(virtual_node)
+    def add(self, item):
+        for index in self._hashes(item):
+            self.bit_array[index] = 1
 
-            self.ring[hashed_key] = node
-            self.sorted_keys.append(hashed_key)
-
-        self.sorted_keys.sort()
-
-    def remove_node(self, node):
-        for i in range(self.replicas):
-            virtual_node = f"{node}:{i}"
-            hashed_key = self._hash(virtual_node)
-
-            del self.ring[hashed_key]
-            self.sorted_keys.remove(hashed_key)
-
-    def get_node(self, key):
-        if not self.ring:
-            return None
-
-        hashed_key = self._hash(key)
-
-        index = bisect.bisect(self.sorted_keys, hashed_key)
-
-        if index == len(self.sorted_keys):
-            index = 0
-
-        return self.ring[self.sorted_keys[index]]
-
+    def might_contain(self, item):
+        for index in self._hashes(item):
+            if self.bit_array[index] == 0:
+                return False
+        return True
 
